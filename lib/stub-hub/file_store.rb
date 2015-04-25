14a61cd27158dfab8408301d7199fccbeb7894dd
Edward Huynh <edward@edwardhuynh.com>
2015-04-25 17:40:47 +1000

    apply formatting to file_store.rb

--- lib/stub-hub/file_store.rb
+++ lib/stub-hub/file_store.rb
@@ -1,37 +1,37 @@
 require 'rubygems'
 
 module Stubhub
-	StubResponse = Struct.new(:contents) do
+  StubResponse = Struct.new(:contents) do
 
-		def empty?
-			contents == nil
-		end
+    def empty?
+      contents == nil
+    end
 
-		def self.empty_response
-			self.new
-		end
+    def self.empty_response
+      self.new
+    end
 
-	end
+  end
 
-	class FileStore
+  class FileStore
 
-		def initialize(seed_directory)
-			@seed_directory = File.expand_path(seed_directory)
-		end
+    def initialize(seed_directory)
+      @seed_directory = File.expand_path(seed_directory)
+    end
 
-		def file_for_uri uri
-			file_name = translate_uri_to_file_name uri
-			file_path = File.join(@seed_directory, file_name)
-			if File.exists?(file_path)
-				contents = File.open(file_path, "r").read
-				StubResponse.new(contents)
-			else
-				StubResponse.empty_response
-			end
-		end
+    def file_for_uri uri
+      file_name = translate_uri_to_file_name uri
+      file_path = File.join(@seed_directory, file_name)
+      if File.exists?(file_path)
+        contents = File.open(file_path, "r").read
+        StubResponse.new(contents)
+      else
+        StubResponse.empty_response
+      end
+    end
 
-		def translate_uri_to_file_name uri
-			uri.sub(/^\//, "").gsub(/\//, "-")
-		end
-	end
+    def translate_uri_to_file_name uri
+      uri.sub(/^\//, "").gsub(/\//, "-")
+    end
+  end
 end
