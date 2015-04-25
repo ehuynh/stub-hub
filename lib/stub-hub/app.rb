20c4c5a3ae61e15d8ae4a3816bf5bba820746222
Edward Huynh <edward@edwardhuynh.com>
2015-04-25 19:31:07 +1000

    rename file_store to response_store
    refactoring in response_store. formatting changes

--- lib/stub-hub/app.rb
+++ lib/stub-hub/app.rb
@@ -1,22 +1,22 @@
 require 'rack'
-require 'stub-hub/file_store'
+require 'stub-hub/response_store'
 
 module Stubhub
-	class App
-		attr_accessor :file_store
+  class App
+    attr_accessor :response_store
 
-		def initialize(seed_directory)
-			@file_store = FileStore.new(seed_directory)
-		end
+    def initialize(seed_directory)
+      @response_store = ResponseStore.new(seed_directory)
+    end
 
-	  def call(env)
-	  	req = Rack::Request.new env
-	  	stub_response = @file_store.file_for_uri req.path_info
-	  	if !stub_response.empty?
-	  		[200, {"Content-Type" => "text/json"}, [stub_response.contents]]
-	  	else
-	  		[404, {}, [""]]
-	  	end
-	  end
-	end
+    def call(env)
+      req = Rack::Request.new env
+      stub_response = @response_store.response_for_uri req.path_info
+      if !stub_response.empty?
+        [200, {"Content-Type" => "text/json"}, [stub_response.contents]]
+      else
+        [404, {}, [""]]
+      end
+    end
+  end
 end
---
+++ lib/stub-hub/response_store.rb
@@ -0,0 +1,49 @@
+require 'rubygems'
+
+module Stubhub
+  StubResponse = Struct.new(:contents) do
+
+    def empty?
+      contents == nil
+    end
+
+    def self.empty_response
+      self.new
+    end
+
+  end
+
+  class ResponseStore 
+
+    def initialize(seed_directory)
+      @seed_directory = File.expand_path(seed_directory)
+    end
+
+    def response_for_uri uri
+      if can_respond? uri then stub_response uri else empty_response end
+    end
+
+    private
+
+    def can_respond? uri
+      File.exists?(file_for_uri uri)
+    end
+
+    def file_for_uri uri
+      File.join(@seed_directory, file_name_for_uri(uri))
+    end
+
+    def file_name_for_uri uri
+      uri.sub(/^\//, "").gsub(/\//, "-")
+    end
+
+    def stub_response uri
+      contents = File.open(file_for_uri(uri), "r").read
+      StubResponse.new(contents)
+    end
+
+    def empty_response
+      StubResponse.empty_response
+    end
+  end
+end
