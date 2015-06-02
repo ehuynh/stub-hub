28bfdf25a9a79a060f88d70607e55de2c22bb645
Edward Huynh <edward@edwardhuynh.com>
2015-06-02 21:59:46 +1000

    add documentation

--- lib/stub-hub/response_store.rb
+++ lib/stub-hub/response_store.rb
@@ -16,6 +16,7 @@ module Stubhub
   
   StubEntry = Struct.new(:file_path)
 
+  # wrapper around the stubhub.yml file
   class StubDefinitions
 
     def initialize(datastore_file)
@@ -23,6 +24,8 @@ module Stubhub
       @yml = Psych.load_file(datastore_file)
     end
 
+    # returns a StubEntry for a given uri.
+    # nil if no stub is defined
     def stub_entry_for_uri uri
       entry = @yml['responses'][uri]
       if entry 
@@ -35,6 +38,7 @@ module Stubhub
 
   end
 
+  # store that returns stubbed responses for a given uri
   class ResponseStore 
 
     def initialize(seed_directory)
@@ -42,6 +46,8 @@ module Stubhub
       @stub_definitions = StubDefinitions.new(File.join(@seed_directory, "stubhub.yml"))
     end
 
+    # returns a stubbed response for a given uri. if no stub has been defined
+    # then it returns an empty stub response
     def response_for_uri uri
       stub_entry = @stub_definitions.stub_entry_for_uri(uri)
       if stub_entry then stub_response(stub_entry) else empty_response end
